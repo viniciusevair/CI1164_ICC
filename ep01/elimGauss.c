c82b4e98ad4f1656577bdcaefa81052e21d78a5d
Vinicius Evair da Silva <Vinicius.Silva@ipinfusion.com>
2024-04-03 20:22:47 -0300

    Adicionei a função de eliminação de Gauss tridiagonal
    Também corrigi alguns nomes da struct tMatrix

--- ep01/elimGauss.c
+++ ep01/elimGauss.c
@@ -8,13 +8,13 @@ void printSolution(struct tMatrix *m, struct tMatrix *originalMatrix) {
     double residual[m->size];
 
     for(i = m->size - 1; i >= 0; i--) {
-        solution[i] = m->sol[i];
+        solution[i] = m->b[i];
         for(j = i + 1; j < m->size; j++)
             solution[i] -= m->data[i][j] * solution[j];
         solution[i] /= m->data[i][i];
     }
     for(i = 0; i < m->size; i++) {
-        residual[i] = (-1) * originalMatrix->sol[i];
+        residual[i] = (-1) * originalMatrix->b[i];
         for(j = 0; j < m->size; j++)
             residual[i] += originalMatrix->data[i][j] * solution[j];
     }
@@ -60,7 +60,22 @@ void gaussianElim(struct tMatrix* m) {
             m->data[row][i] = 0.0;
             for(int col = i + 1; col < m->size; col++)
                 m->data[row][col] -= m->data[i][col] * multiplier;
-            m->sol[row] -= m->sol[i] * multiplier;
+            m->b[row] -= m->b[i] * multiplier;
         }
     }
+}
+
+void gaussianElimTrid(double *d, double *a, double *c, double *b,
+                      double *x, unsigned int n) {
+    for(int i = 0; i < n - 1; i++) {
+        double multiplier = a[i] / d[i];
+
+        a[i] = 0.0;
+        d[i+1] -= c[i] * multiplier;
+        b[i+1] -= b[i] * multiplier;
+    }
+
+    x[ n-1 ] = b[ n-1 ] / d[ n-1 ];
+    for (int i=n-2; i >= 0; --i)
+        x[ i ] = (b[ i ] - c[ i ] * x[ i+1 ]) / d[ i ];
 }
--- ep01/elimGauss.h
+++ ep01/elimGauss.h
@@ -1,3 +1,4 @@
-void gaussianElim(struct tMatrix* m, double *b);
+void gaussianElim(struct tMatrix* m);
 
-void gaussianElimTrid(struct tMatrix* m);
+void gaussianElimTrid(double *d, double *a, double *c, double *b,
+                      double *x, unsigned int n);
