aa406760d9603f5fc5d4ada1b118d340300493e5
Vinicius Evair da Silva <Vinicius.Silva@ipinfusion.com>
2024-04-05 14:17:09 -0300

    Arrumei para rodar com likwid

--- ep01/main.c
+++ ep01/main.c
@@ -1,10 +1,13 @@
+#include <likwid-marker.h>
 #include <stdio.h>
+#include <likwid.h>
 #include "linSys.h"
 #include "elimGauss.h"
 #include "gaussSeidel.h"
 #include "utils.h"
 
 int main(int argc, char *argv[]) {
+    LIKWID_MARKER_INIT;
     rtime_t tempo;
     int matrixSize;
     scanf("%d", &matrixSize);
@@ -16,10 +19,12 @@ int main(int argc, char *argv[]) {
     struct tMatrix *n = makeCopy(m);
     double *x = createArray(matrixSize);
     double *r = createArray(matrixSize);
+    LIKWID_MARKER_START("eg_classico");
     tempo = timestamp();
     gaussianElim(n);
     getSolution(n, x);
     tempo = timestamp() - tempo;
+    LIKWID_MARKER_STOP("eg_classico");
     getResidual(x, r, m);
 
     printf("EG clássico:\n");
@@ -37,9 +42,11 @@ int main(int argc, char *argv[]) {
     int it;
     x = createArray(matrixSize);
     r = createArray(matrixSize);
+    LIKWID_MARKER_START("gs_classico");
     tempo = timestamp();
     it = gaussSeidel(m, x, 10e-4);
     tempo = timestamp() - tempo;
+    LIKWID_MARKER_STOP("gs_classico");
     getResidual(x, r, m);
 
     printf("GS clássico [ %d iterações ]:\n", it);
@@ -57,10 +64,12 @@ int main(int argc, char *argv[]) {
     struct tTridMatrix *o = makeTridCopy(m);
     x = createArray(matrixSize);
     r = createArray(matrixSize);
+    LIKWID_MARKER_START("eg_trid");
     tempo = timestamp();
     gaussianElimTrid(o->d, o->a, o->c, o->b, x, o->size);
     getTridSolution(o, x);
     tempo = timestamp() - tempo;
+    LIKWID_MARKER_STOP("eg_trid");
     getResidual(x, r, m);
 
     printf("EG 3-diagonal:\n");
@@ -78,9 +87,11 @@ int main(int argc, char *argv[]) {
     struct tTridMatrix *p = makeTridCopy(m);
     x = createArray(matrixSize);
     r = createArray(matrixSize);
+    LIKWID_MARKER_START("gs_trid");
     tempo = timestamp();
     it = gaussSeidelTrid(p->d, p->a, p->c, p->b, x, p->size, 10e-4);
     tempo = timestamp() - tempo;
+    LIKWID_MARKER_STOP("gs_trid");
     getResidual(x, r, m);
 
     printf("GS 3-diagonal [ %d iterações ]:\n", it);
@@ -89,5 +100,6 @@ int main(int argc, char *argv[]) {
     printArray(r, matrixSize);
     printf("\n");
 
+    LIKWID_MARKER_CLOSE;
     return 0;
 }
