b364469844986bdd48708de294dc28f6d805a67d
Vinicius Evair <vsilva.evair@gmail.com>
2024-04-06 20:04:34 -0300

    Comentários adicionados ao script

--- ep01/script.sh
+++ ep01/script.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-parse_output () {
-    PROG_OUTPUT="$1"
-    FILTERED_OUTPUT="$2"
+##############################################
+# Script feito por Vinicius Evair da Silva e #
+#   Luiz Henrique Murback Wiedmer para a     #
+#  disciplina de Introdução à Computação     #
+#           Científica (CI1164)              #
+##############################################
+#
+# Este script executa o programa 'perfSL' com diversos sistemas lineares como
+# entrada, e utiliza o likwid-perfctr para medir e filtrar as taxas de
+# desempenho em MFLOP/s (megaflops por segundo). Os resultados de cada sistema
+# linear são armazenados em arquivos de saída separados.
 
+
+# Função que filtra a saída do likwid para extraír apenas MFLOP/s
+parse_output () {
+    PROG_OUTPUT="$1"        # Saída do programa 'perfSL'.
+    FILTERED_OUTPUT="$2"    # Arquivo de saída final.
+
+    # Função que reorganiza a saída do programa e a saída do likwid de cada
+    # método. Também filtra a saída do likwid, guardando apenas o MFLOP/s
     filter_results () {
         PATTERN="$1"
         LABEL="$2"
@@ -13,6 +29,7 @@ parse_output () {
         echo " " >> ${FILTERED_OUTPUT}
     }
 
+    # Limpa o arquivo de saída caso este já exista.
     if [ -f "${FILTERED_OUTPUT}" ]; then
         rm -f "${FILTERED_OUTPUT}"
     fi
@@ -29,7 +46,10 @@ if [ ! -d "outputs" ]; then
     mkdir "outputs"
 fi
 
+# Cria um arquivo temporário que será utilizado como saída do programa para ser
+# filtrado pela função acima.
 TEMP_FILE=$(mktemp -p .)
+
 # Itera por todos os arquivos dentro do diretório de entradas.
 # Cada arquivo contém um dos exemplos do arquivo sistemas.dat.
 for INPUT_FILE in inputs/input*; do
@@ -43,4 +63,5 @@ for INPUT_FILE in inputs/input*; do
     parse_output "$TEMP_FILE" "$OUTPUT_FILE"
 done
 
+# Remove o arquivo auxiliar temporário.
 rm $TEMP_FILE
